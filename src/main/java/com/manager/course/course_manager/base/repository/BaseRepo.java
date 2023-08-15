9720ac3e96ac15cbd4d81c56cc94eef691121b79
top <mobinaamerion@gmail.com>
2023-08-15 10:29:08 +0430

    modify in base repo

--- src/main/java/com/manager/course/course_manager/base/repository/BaseRepo.java
+++ src/main/java/com/manager/course/course_manager/base/repository/BaseRepo.java
@@ -3,13 +3,14 @@ package com.manager.course.course_manager.base.repository;
 import com.manager.course.course_manager.base.Base;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.io.Serializable;
 
-
+@Repository
 @Transactional
-public interface BaseRepo<T extends Base<ID>, ID extends Serializable> extends JpaRepository<T, ID> {
+public interface BaseRepo<T extends Base<ID>, ID extends Serializable> extends CrudRepository<T, ID> {
 
 
 }
